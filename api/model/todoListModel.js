839f0c6d450c56d26e55c67717089241ddebb2a4
Opendarasa <ouedson0128@yahoo.fr>
2018-08-27 15:12:06 +0800

    initial commit

---
+++ api/model/todoListModel.js
@@ -0,0 +1,30 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+/*
+ * @creating db.Tasks schema 
+ * @Author :jean Ouedraogo
+ * @date : 27-08-2018
+ *
+ */
+
+var TaskSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the task'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed']
+    }],
+    default: ['pending']
+  }
+});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
